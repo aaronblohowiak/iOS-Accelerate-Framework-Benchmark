814ffe06968056b354b58f36da8f4ec859b4a795
Aaron Blohowiak <aaron.blohowiak@gmail.com>
2012-02-20 10:29:00 -0800

    Initial Commit

---
+++ accelerate-benchmark/XitiveAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  XitiveAppDelegate.h
+//  accelerate-benchmark
+//
+//  Created by Aaron Blohowiak on 2/20/12.
+//  Copyright (c) 2012 none. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class XitiveViewController;
+
+@interface XitiveAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) XitiveViewController *viewController;
+
+@end
---
+++ accelerate-benchmark/XitiveViewController.h
@@ -0,0 +1,13 @@
+//
+//  XitiveViewController.h
+//  accelerate-benchmark
+//
+//  Created by Aaron Blohowiak on 2/20/12.
+//  Copyright (c) 2012 none. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XitiveViewController : UIViewController
+
+@end
